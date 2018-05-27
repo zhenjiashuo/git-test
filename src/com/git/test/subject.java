77378889cc745cbc313b8facbc4c8c179f44b2db
zhenjiashuo <403311829@qq.com>
2018-05-27 12:58:39 +0800

    test

--- src/com/git/test/subject.java
+++ src/com/git/test/subject.java
@@ -3,11 +3,7 @@ package com.git.test;
 public class subject {
 	
 	public void method() {
-<<<<<<< HEAD
-		String test = "22222";
-=======
 		String test = "11111";
->>>>>>> refs/remotes/origin/master
 		int testInt = 33-1;
 	}
 
